f1dac5a3eb2a849ee45329b6bb4c84a328ce32b2
wdy <1959239160@qq.com>
2021-10-13 16:23:40 +0800

    first commit

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderConsulController {
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+    @Autowired
+    private RestTemplate restTemplate;
+    @RequestMapping("/consumer/payment/consul")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+        System.out.println("消费者调用支付服务(zookeeper)--->result:"+result);
+        return result;
+    }
+}
